227544600d13709ce6af36ae7768b90cb6ec7ca6
Olivier Scharff <scharff.olivier@gmail.com>
2025-12-08 18:51:47 +0000

    day08 part 2

--- Day08.php
+++ Day08.php
@@ -23,11 +23,10 @@ asort($distances);
 $nbGroups = 3;
 $nbDistances = 1000;
 
-array_splice($distances, $nbDistances);
-
 $groups = [];
 
-// displayGroups($groups);
+$count = 0;
+$part2 = 0;
 foreach ($distances as $key => $distance) {
     list($i, $j) = explode('-', $key);
     // echo "On check le couple $i, $j\n";
@@ -40,28 +39,39 @@ foreach ($distances as $key => $distance) {
 
     if ($a !== false xor $b !== false) {
         if ($a !== false) {
-            $groups[$a['groupNumber']][] = $j;
+            $groups[$a][] = $j;
         }
         if ($b !== false) {
-            $groups[$b['groupNumber']][] = $i;
+            $groups[$b][] = $i;
         }
     }
 
-    if ($a !== false && $b !== false && $a['groupNumber'] != $b['groupNumber']) {
+    if ($a !== false && $b !== false && $a != $b) {
         // on merge les deux groupes.
-        $groups[$a['groupNumber']] = array_unique(array_merge($groups[$a['groupNumber']], $groups[$b['groupNumber']]));
-        unset($groups[$b['groupNumber']]);
+        $groups[$a] = array_unique(array_merge($groups[$a], $groups[$b]));
+        unset($groups[$b]);
     }
     // displayGroups($groups);
     // readline('next ?');
+    $count++;
+    if ($nbDistances == $count) answerPart1($groups, $nbGroups);
+    if (count(end($groups)) == count($points)) {
+        $part2 = $points[$i][0] * $points[$j][0];
+        break;
+    }
 }
 
-$product = 1;
-usort($groups, fn($a, $b) => count($b) <=> count($a));
-for($i = 0; $i < $nbGroups; $i++){
-    $product *= count($groups[$i]);
+echo "Part 2 : $part2\n";
+
+function answerPart1($groups, $nbGroups)
+{
+    $product = 1;
+    usort($groups, fn($a, $b) => count($b) <=> count($a));
+    for ($i = 0; $i < $nbGroups; $i++) {
+        $product *= count($groups[$i]);
+    }
+    echo "Part 1 : $product\n";
 }
-echo "Part 1 : $product\n";
 
 function displayGroups(array $groups)
 {
@@ -82,10 +92,8 @@ function inWhichGroup(array $groups, int $i, int $j): array
     $a = false;
     $b = false;
     foreach ($groups as $groupNumber => $group) {
-        $indexA = array_search($i, $group);
-        $indexB = array_search($j, $group);
-        if ($indexA !== false) $a = ['groupNumber' => $groupNumber, 'key' => $indexA];
-        if ($indexB !== false) $b = ['groupNumber' => $groupNumber, 'key' => $indexB];
+        $a = in_array($i, $group) ? $groupNumber : $a;
+        $b = in_array($j, $group) ? $groupNumber : $b;
         if ($a !== false && $b !== false) break;
     }
     return [$a, $b];
